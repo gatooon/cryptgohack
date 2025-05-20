0905e56dfd71c6ddd3b86ba857ad0badeebd5191
gatooon <gatoon.golitin@proton.me>
2025-05-20 11:07:49 +0200

    legendr add bigint

--- modular-arithmetic/legendr/main.go
+++ modular-arithmetic/legendr/main.go
@@ -1,55 +1,76 @@
 package main
 
 import (
-	"bufio"
+//	"bufio"
 	"errors"
 	"fmt"
 	"log"
-	"math"
+//	"math"
 	"math/big"
 	"os"
-	"strings"
+//	"strings"
 )
 
 func main(){
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		log.Fatal("Argument is needed")
 	}
 
- 	file, err := os.Open(os.Args[1])
-    if err != nil {
-        fmt.Println(err)
-    }
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	var p_string, numbers_string string
-    for scanner.Scan() {
-		if len(scanner.Text()) != 0 {
-			if scanner.Text()[0] == []byte("p")[0]{
-				p_string = strings.Split(scanner.Text(), " = ")[1]
-			}
-			if scanner.Text()[0] == []byte("i")[0]{
-				numbers_string = strings.Split(scanner.Text(), " = ")[1]
-			}
-		}
-    }
-
-	numbers_string = numbers_string[1:]
-	numbers_string = numbers_string[:len(numbers_string)-1]
-
 	p := new(big.Int)
-	p, ok := p.SetString(p_string, 10)
+	p, ok := p.SetString(os.Args[1], 10)
 	if !ok {
 		log.Fatal("Error Occured")
 	}
 
-	for _, number_string := range(strings.Split(numbers, ", ")){
-
-		number_string
-
-		fmt.Printf("[o] p = %v ; a = %v\n", p, number)
-		
+	a := new(big.Int)
+	a, ok = p.SetString(os.Args[2], 10)
+	if !ok {
+		log.Fatal("Error Occured")
 	}
+
+
+	value, err := GetSquareRoot(a, p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(value)
+
+
+
+// 	file, err := os.Open(os.Args[1])
+//    if err != nil {
+//        fmt.Println(err)
+//    }
+//	defer file.Close()
+//	scanner := bufio.NewScanner(file)
+//	var p_string, numbers_string string
+//    for scanner.Scan() {
+//		if len(scanner.Text()) != 0 {
+//			if scanner.Text()[0] == []byte("p")[0]{
+//				p_string = strings.Split(scanner.Text(), " = ")[1]
+//			}
+//			if scanner.Text()[0] == []byte("i")[0]{
+//				numbers_string = strings.Split(scanner.Text(), " = ")[1]
+//			}
+//		}
+//    }
+//
+//	numbers_string = numbers_string[1:]
+//	numbers_string = numbers_string[:len(numbers_string)-1]
+//
+//	p := new(big.Int)
+//	p, ok := p.SetString(p_string, 10)
+//	if !ok {
+//		log.Fatal("Error Occured")
+//	}
+//
+//	for _, number_string := range(strings.Split(numbers, ", ")){
+//
+//		number_string
+//
+//		fmt.Printf("[o] p = %v ; a = %v\n", p, number)
+//		
+//	}
     
 
 	//	err, square_root := GetSquareRoot(a, p)
@@ -61,24 +82,38 @@ func main(){
 	//
 }
 
-func CheckQResidue(a BigInteger, p BigInteger) bool{
-	res := int(math.Pow(a, (p-1)/2))%int(p)
-	if res == 1 {
+func CheckQResidue(a *big.Int, p *big.Int) bool{
+	//res := int(math.Pow(a, (p-1)/2))%int(p)
+
+	res := big.NewInt(0).Sub(p, big.NewInt(1))
+	fmt.Println(res.String())
+	res = big.NewInt(0).Div(res, big.NewInt(2))
+	fmt.Println(res.String())
+	res = big.NewInt(0).Exp(a, res, nil)
+
+
+	if res.String() == "1" {
 		return true
 	}
 	return false
 }
 
-func GetSquareRoot(a float64, p float64) (error,int) {
+func GetSquareRoot(a *big.Int, p *big.Int) (*big.Int, error) {
 	
 	if CheckQResidue(a, p) != true {
-		return errors.New("Can't apply algorithm, non quadratic residue"), 0
+		return big.NewInt(0), errors.New("Can't apply algorithm, non quadratic residue")
 	}
 
 
-	if int(p)%4 != 3 {
-		return errors.New("Can't apply algorithm, p != 3 mod 4"), 0
+	if big.NewInt(0).Mod(p, big.NewInt(4)).String() != "3" {
+		return big.NewInt(0), errors.New("Can't apply algorithm, p != 3 mod 4")
 	}
 
-	return nil, int(math.Pow(a, (p+1)/4))%int(p)
+	
+	exponent := big.NewInt(0).Add(p, big.NewInt(1))
+	exponent = big.NewInt(0).Div(exponent, big.NewInt(4))
+	value := big.NewInt(0).Exp(a, exponent, nil)
+	value = big.NewInt(0).Mod(value, p)
+
+	return value, nil
 }
