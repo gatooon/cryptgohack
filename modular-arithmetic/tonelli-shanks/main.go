5cc1839bd837a1d78edf3cc75443dcaa79209b63
gatooon <gatoon.golitin@proton.me>
2025-05-27 10:24:50 +0200

    tonelli-shanks added

--- modular-arithmetic/tonelli-shanks/main.go
+++ modular-arithmetic/tonelli-shanks/main.go
@@ -36,16 +36,11 @@ func main(){
 
 
 func TonelliShanks(a *big.Int, p *big.Int) *big.Int{
-	big_int := big.NewInt(0)
-	Q := big_int.Sub(p, big.NewInt(1))
-	fmt.Println(Q)
+	Q := big.NewInt(0).Sub(p, big.NewInt(1))
 	S := big.NewInt(0)
-	fmt.Println(S)
-	for big_int.Mod(Q, big.NewInt(2)).String() == "0" {
+	for big.NewInt(0).Mod(Q, big.NewInt(2)).String() == "0" {
 		S = big.NewInt(0).Add(S, big.NewInt(1))
 		Q = big.NewInt(0).Div(Q, big.NewInt(2))
-		fmt.Println(Q)
-		fmt.Println(S)
 		if Q.String() == "0" {
 			log.Fatal("error")
 		}
@@ -56,30 +51,30 @@ func TonelliShanks(a *big.Int, p *big.Int) *big.Int{
 
 	z := big.NewInt(2)
 	for CheckQResidue(z, p) != false {
-		z = big_int.Add(z, big.NewInt(1))
+		z = big.NewInt(0).Add(z, big.NewInt(1))
 	}
 	fmt.Printf("[o] non quadratic residu for tonelli-shanks if %v\n", z.String())
 
 	M := S
-	c := big_int.Exp(z, Q, p)
-	t := big_int.Exp(a, Q, p)
-	R := big_int.Exp(a, big_int.Div(big_int.Add(Q, big.NewInt(1)), big.NewInt(2)), p)
+	c := big.NewInt(0).Exp(z, Q, p)
+	t := big.NewInt(0).Exp(a, Q, p)
+	R := big.NewInt(0).Exp(a, big.NewInt(0).Div(big.NewInt(0).Add(Q, big.NewInt(1)), big.NewInt(2)), p)
 
 	for t.String() != "1" {
 		i := big.NewInt(0)
 		tmp := t
 		for tmp.String() != "1" {
-			i = big_int.Add(i, big.NewInt(1))
-			tmp = big_int.Mod(big_int.Mul(tmp, tmp), p)
+			i = big.NewInt(0).Add(i, big.NewInt(1))
+			tmp = big.NewInt(0).Mod(big.NewInt(0).Mul(tmp, tmp), p)
 		}
 
-		pow2 := big_int.Exp(big.NewInt(2), big_int.Sub(big_int.Sub(M, i), big.NewInt(1)), nil)
-		b := big_int.Exp(c, pow2, p)
+		pow2 := big.NewInt(0).Exp(big.NewInt(2), big.NewInt(0).Sub(big.NewInt(0).Sub(M, i), big.NewInt(1)), nil)
+		b := big.NewInt(0).Exp(c, pow2, p)
 
 		M = i
-		c = big_int.Exp(b, big.NewInt(2), p)
-		t = big_int.Mod(big_int.Mul(big_int.Mul(t, b), b), p)
-		R = big_int.Mod(big_int.Mul(R, b), p)
+		c = big.NewInt(0).Exp(b, big.NewInt(2), p)
+		t = big.NewInt(0).Mod(big.NewInt(0).Mul(big.NewInt(0).Mul(t, b), b), p)
+		R = big.NewInt(0).Mod(big.NewInt(0).Mul(R, b), p)
 	}
 
 	return R
