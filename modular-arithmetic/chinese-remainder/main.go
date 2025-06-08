1ea83b73f2f160d2b571c8e0d2c017ff92fb11b6
gatooon <gatoon.golitin@proton.me>
2025-06-08 15:53:37 +0200

    chinese remainder

--- modular-arithmetic/chinese-remainder/main.go
+++ modular-arithmetic/chinese-remainder/main.go
@@ -5,17 +5,20 @@ import (
 )
 
 func main(){
-	fmt.Printf("[o]%v\n", ExtendedEuclidian(13, 11))
+	slice := [][]int{{2, 5}, {3, 11}, {5, 17}}
+	fmt.Printf("[o]%v\n", ChineseRemainder(slice))
 }
 
 func ExtendedEuclidian(a int, b int) int {
-	fmt.Printf("[o]Extended Euclidian Algorithm\n")
+	input_a, input_b := a, b
 	var mod_result, t, q int
 	t1 := 0
 	t2 := 1
 	for {
-		fmt.Printf("[o][o] a = %v, b = %v, q = %v, mod = %v, t1 = %v, t2 = %v, t = %v\n", a, b, q, mod_result, t1, t2, t)
+//		fmt.Printf("[o][o] a = %v, b = %v, q = %v, mod = %v, t1 = %v, t2 = %v, t = %v\n", a, b, q, mod_result, t1, t2, t)
 		if b == 0 {
+			t1 = modNegative(t1, input_a)
+			fmt.Printf("[o] Modular Inverse for %v mod %v is %v\n", input_b, input_a, t1)
 			return t1
 		}
 		q = a/b
@@ -26,7 +29,28 @@ func ExtendedEuclidian(a int, b int) int {
 		mod_result = a%b
 		a = b
 		b = mod_result
-
-
 	}
+}
+
+func ChineseRemainder(given [][]int) int {
+	M := 1
+	var M1, M_1, output int
+	for _, element := range given {
+		M = M*element[1]
+	}
+
+	fmt.Printf("[o]M = %v\n", M)
+
+	for _, element := range given {
+		M1 = M/element[1]
+		M_1 = ExtendedEuclidian(element[1], M1)
+		output = output + (element[0] * M1 * M_1)
+		fmt.Printf("[o]a = %v, Mx = %v, Modular Inverse = %v, output = %v\n", element[0], M1, M_1, output)
+	}
+
+	return output%M
+}
+
+func modNegative(a int, b int) int{
+	return (a % b + b) % b
 }
